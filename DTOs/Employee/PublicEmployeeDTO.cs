36c3e644075e8379b7db7698253b7f0d5809f000
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2024-11-20 11:04:48 +0100

    added public employee dto

---
+++ DTOs/Employee/PublicEmployeeDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DTOs;
+
+public class PublicEmployeeDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int WorkingNumber { get; set; }
+}
