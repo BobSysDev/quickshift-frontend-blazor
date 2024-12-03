33d27231866f4812e05973ae93365de67cc34404
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2024-12-03 15:28:27 +0100

    Now all dtos match backend

--- DTOs/Employee/ShiftEmpoyeeDTO.cs
+++ DTOs/Employee/ShiftEmpoyeeDTO.cs
@@ -2,14 +2,11 @@
 
 namespace DTOs;
 
-public class EmployeeDTO
+public class ShiftEmpoyeeDTO
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public int WorkingNumber { get; set; }
     public long Id { get; set; }
-    public string Email { get; set; }
     public List<ShiftDTO> Shifts { get; set; }
-    public string Password { get; set; }
-
 }
