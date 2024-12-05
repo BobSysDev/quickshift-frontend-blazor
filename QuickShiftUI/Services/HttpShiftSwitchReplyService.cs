bf5f5736e997f6a1f014d902620dc908e57d75eb
Karolina Krysiak <karolinakrysiak01@gmail.com>
2024-12-05 14:40:26 +0100

    services done

--- QuickShiftUI/Services/HttpShiftSwitchReplyService.cs
+++ QuickShiftUI/Services/HttpShiftSwitchReplyService.cs
@@ -7,12 +7,12 @@ namespace QuickShiftUI.Services;
 public class HttpShiftSwitchReplyService : IShiftSwitchReplyService
 {
     private readonly HttpClient _httpClient;
-    
+
     public HttpShiftSwitchReplyService(HttpClient httpClient)
     {
         _httpClient = httpClient;
     }
-    
+
     public async Task CreateShiftSwitchReplyAsync(long requestId, NewShiftSwitchReplyDTO shiftSwitchReplyDto)
     {
         await _httpClient.PostAsJsonAsync("ShiftSwitching/Request/{requestId}/Reply", shiftSwitchReplyDto);
@@ -28,20 +28,49 @@ public class HttpShiftSwitchReplyService : IShiftSwitchReplyService
         return await _httpClient.GetFromJsonAsync<ShiftSwitchReplyDTO>("ShiftSwitching/Reply/{ID}");
     }
 
-    public async Task<ShiftSwitchReplyDTO> UpdateShiftSwitchReplyAsync(long Id, UpdateShiftSwitchReplyDTO shiftSwitchReplyDto)
+    public async Task<ShiftSwitchReplyDTO> UpdateShiftSwitchReplyAsync(long Id,
+        UpdateShiftSwitchReplyDTO shiftSwitchReplyDto)
     {
-        HttpResponseMessage response = await _httpClient.PatchAsJsonAsync($"ShiftSwitching/Reply/{Id}", shiftSwitchReplyDto);
-        ShiftSwitchReplyDTO responseDTO = JsonSerializer.Deserialize<ShiftSwitchReplyDTO>(await response.Content.ReadAsStringAsync());
+        HttpResponseMessage response =
+            await _httpClient.PatchAsJsonAsync($"ShiftSwitching/Reply/{Id}", shiftSwitchReplyDto);
+        ShiftSwitchReplyDTO responseDTO =
+            JsonSerializer.Deserialize<ShiftSwitchReplyDTO>(await response.Content.ReadAsStringAsync());
         return responseDTO;
     }
 
     public async Task DeleteShiftSwitchReplyAsync(long Id)
     {
-        await _httpClient.DeleteAsync("ShiftSwitching/Reply/{Id}");
+        await _httpClient.DeleteAsync($"ShiftSwitching/Reply/{Id}");
     }
 
     public async Task<ShiftSwitchReplyDTO> GetShiftSwitchReplyByEmployeeIdAsync(long employeeId)
     {
         return await _httpClient.GetFromJsonAsync<ShiftSwitchReplyDTO>($"Employee/{employeeId}/ShiftSwitching/Reply");
+    }
+
+    public async Task TargetRejectShiftSwitchReplyAsync(long requestId, long replyId)
+    {
+        await _httpClient.DeleteAsync($"/ShiftSwitching/Request/{requestId}/Reply/{replyId}/TargetAccept");
+    }
+
+    public async Task TargetAcceptShiftSwitchReplyAsync(long requestId, long replyId)
+    {
+        await _httpClient.PostAsJsonAsync($"/ShiftSwitching/Request/{requestId}/Reply/{replyId}/TargetAccept", "");
+    }
+
+    public async Task OriginRejectShiftSwitchReplyAsync(long requestId, long replyId)
+    {
+        await _httpClient.DeleteAsync($"/ShiftSwitching/Request/{requestId}/Reply/{replyId}/OriginAccept");
+    }
+
+    public async Task OriginAcceptShiftSwitchReplyAsync(long requestId, long replyId)
+    {
+        await _httpClient.PostAsJsonAsync($"/ShiftSwitching/Request/{requestId}/Reply/{replyId}/OriginAccept", "");
+    }
+
+    public async Task<List<ShiftDTO>> SwitchShiftsAsync(long requestId, long replyId)
+    {
+        return await _httpClient.GetFromJsonAsync<List<ShiftDTO>>(
+            $"/ShiftSwitching/Request/{requestId}/Reply/{replyId}/Resolve");
     }
 }
--- QuickShiftUI/Services/IShiftSwitchReplyService.cs
+++ QuickShiftUI/Services/IShiftSwitchReplyService.cs
@@ -7,15 +7,16 @@ public interface IShiftSwitchReplyService
 {
     Task CreateShiftSwitchReplyAsync(long requestId, NewShiftSwitchReplyDTO shiftSwitchReplyDto);
     Task<ShiftSwitchReplyDTO> GetShiftSwitchReplyByRequestIdAsync(long requestId);
-    Task<ShiftSwitchReplyDTO> GetShiftSwitchReplyByIdAsync(long Id);
-    Task<ShiftSwitchReplyDTO> UpdateShiftSwitchReplyAsync(long Id, UpdateShiftSwitchReplyDTO shiftSwitchReplyDto);
-    Task DeleteShiftSwitchReplyAsync(long Id);
+    Task<ShiftSwitchReplyDTO> GetShiftSwitchReplyByIdAsync(long replyId);
+    Task<ShiftSwitchReplyDTO> UpdateShiftSwitchReplyAsync(long replyId, UpdateShiftSwitchReplyDTO shiftSwitchReplyDto);
+    Task DeleteShiftSwitchReplyAsync(long replyId);
     
     Task<ShiftSwitchReplyDTO> GetShiftSwitchReplyByEmployeeIdAsync(long employeeId);
+
+    Task TargetRejectShiftSwitchReplyAsync(long requestId, long replyId);
+    Task TargetAcceptShiftSwitchReplyAsync(long requestId, long replyId);
+    Task OriginRejectShiftSwitchReplyAsync(long requestId, long replyId);
+    Task OriginAcceptShiftSwitchReplyAsync(long requestId, long replyId);
     
-    //
-    //
-    //
-    //
-    //
+    Task<List<ShiftDTO>> SwitchShiftsAsync(long requestId, long replyId);
 }
