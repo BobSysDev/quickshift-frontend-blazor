4746c837c0b09d140e1389e1f8e37aff404aaec7
Marius Gafton <mariusgafton2005@gmail.com>
2024-12-10 16:46:02 +0100

    changed the design a bit (mostly played with it  :) )

--- QuickShiftUI/Services/HttpShiftService.cs
+++ QuickShiftUI/Services/HttpShiftService.cs
@@ -14,29 +14,29 @@ public class HttpShiftService : IShiftService
     
     public async Task<ShiftDTO> GetShiftByIdAsync(long Id)
     {
-        return await _httpClient.GetFromJsonAsync<ShiftDTO>($"MyShifts/{Id}");
+        return await _httpClient.GetFromJsonAsync<ShiftDTO>($"Shift/{Id}");
     }
     
     public async Task<IEnumerable<ShiftDTO>> GetAllShiftsAsync()
     {
-        return await _httpClient.GetFromJsonAsync<IEnumerable<ShiftDTO>>($"MyShifts");
+        return await _httpClient.GetFromJsonAsync<IEnumerable<ShiftDTO>>($"Shift");
     }
     
     public async Task AddShiftAsync(CreateShiftDTO shiftDto)
     {
-        await _httpClient.PostAsJsonAsync("MyShifts", shiftDto);
+        await _httpClient.PostAsJsonAsync("Shift", shiftDto);
     }
 
     public async Task<ShiftDTO> UpdateShiftAsync(long Id, CreateShiftDTO shiftDto)
     {
-        HttpResponseMessage response = await _httpClient.PatchAsJsonAsync($"MyShifts/{Id}", shiftDto);
+        HttpResponseMessage response = await _httpClient.PatchAsJsonAsync($"Shift/{Id}", shiftDto);
         ShiftDTO responseDTO = JsonSerializer.Deserialize<ShiftDTO>(await response.Content.ReadAsStringAsync());
         return responseDTO;
     }
 
     public async Task DeleteShiftAsync(long Id)
     {
-        await _httpClient.DeleteAsync($"MyShifts/{Id}");
+        await _httpClient.DeleteAsync($"Shift/{Id}");
     }
     
     
