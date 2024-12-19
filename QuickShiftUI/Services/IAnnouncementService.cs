2544b9cf34557bd60aa86b4fdaa63ed84dc4a567
SebinoBR <sbrbartko@gmail.com>
2024-12-19 14:21:42 +0100

    added buttons last of them and made it so it only shows 10 announcements ordered chronologically (?)

--- QuickShiftUI/Services/IAnnouncementService.cs
+++ QuickShiftUI/Services/IAnnouncementService.cs
@@ -5,6 +5,8 @@ namespace QuickShiftUI.Services
     public interface IAnnouncementService
     {
         Task<List<AnnouncementDTO>> GetAllAnnouncementsAsync();
+        Task<List<AnnouncementDTO>> GetMostRecentAnnouncementsAsync(int amount);
+
         Task<AnnouncementDTO> GetAnnouncementByIdAsync(long id);
         Task AddAnnouncementAsync(AnnouncementDTO announcement);
         Task<AnnouncementDTO> UpdateAnnouncementAsync(long id, AnnouncementDTO announcement);
